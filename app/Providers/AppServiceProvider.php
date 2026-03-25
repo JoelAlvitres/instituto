fa0ad9c7311d644e27f6b928f9564aadc91568ce
JoelAlvitres <angelalvitresrodas@gmail.com>
2026-03-25 02:16:47 -0500

    Fix AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Closure;
+use Filament\Tables\Columns\ImageColumn;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /*
+         * Filament: ImageColumn no incluye formatStateUsing (solo TextColumn vía CanFormatState).
+         * Compatibilidad: redirigir a getStateUsing pasando el estado del campo desde el registro.
+         */
+        if (! ImageColumn::hasMacro('formatStateUsing')) {
+            ImageColumn::macro('formatStateUsing', function (Closure $callback): ImageColumn {
+                /** @var ImageColumn $this */
+                return $this->getStateUsing(function ($record) use ($callback) {
+                    $state = data_get($record, $this->getName());
+
+                    return $callback($state);
+                });
+            });
+        }
     }
 }
