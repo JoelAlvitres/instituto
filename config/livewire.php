0cb265b459785882571ba554c5aa4cb3df618025
JoelAlvitres <angelalvitresrodas@gmail.com>
2026-04-20 02:29:39 -0500

    Ajusta Livewire para subida de PDF y actualiza biblioteca

--- config/livewire.php
+++ config/livewire.php
@@ -64,14 +64,15 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
+        'disk' => 'public',
         'rules' => ['required', 'file', 'max:153600'], // 150 MiB; Livewire temporal uploads (kilobytes)
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
-        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
+        'preview_mimes' => [
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+            'pdf',
         ],
         'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
         'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
